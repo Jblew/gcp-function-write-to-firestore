9dc969646ff83a677c69d40a91c34dbaa4c3c95a
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2020-06-23 22:07:00 +0200

    ci: build pipeline

--- project.config.sh
+++ project.config.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-source "${PROJECT_DIR}/secrets.config.sh"
-
 export GCP_PROJECT_ID=""
 export GCP_FUNCTIONS_REGION="europe-west3"
-export FIRESTORE_COL_PATH="/log"
+export FIRESTORE_COLLECTION="/log"
+export API_SECRET_KEYS="aaaaa"
